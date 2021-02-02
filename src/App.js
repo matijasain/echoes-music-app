38b71746345393aa7406d2ad6d242ec2e6827137
Matija <sain.ms@pg.com>
2021-02-02 08:35:53 +0100

    initial components | player icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import './styles/app.scss';
+
+import "./styles/app.scss";
+import Song from "./components/Song";
+import Player from "./components/Player";
+
 
 function App() {
   return (
     <div className="App">
-      <h1>Music player</h1>
+      <Song />
+      <Player />
     </div>
   );
 }
