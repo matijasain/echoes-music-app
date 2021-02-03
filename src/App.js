1909106d268d1a19d5ccc020e68d7f50cbbf84a5
Matija Sain <matijasain@Matijas-MacBook-Air.local>
2021-02-03 07:50:32 +0100

    styling | data | logic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-
-import "./styles/app.scss";
-import Song from "./components/Song";
-import Player from "./components/Player";
-
+import { useState } from 'react';
+import './styles/app.scss';
+import Song from './components/Song';
+import Player from './components/Player';
+import data from './util';
 
 function App() {
+  const [songs, setSongs] = useState(data());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player currentSong={currentSong} />
     </div>
   );
 }
--- src/components/Player.js
+++ src/components/Player.js
@@ -1,8 +1,18 @@
-import React from "react";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faPlay ,faAngleLeft, faAngleRight} from "@fortawesome/free-solid-svg-icons"
+import React, { useRef } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faPlay,
+  faAngleLeft,
+  faAngleRight,
+} from '@fortawesome/free-solid-svg-icons';
 
-const Player = () => {
+const Player = ({ currentSong }) => {
+  const audioRef = useRef(null);
+
+  const playSongHandler = () => {
+    console.log(audioRef);
+  };
+
   return (
     <div className="player">
       <div className="time-control">
@@ -11,10 +21,20 @@ const Player = () => {
         <p>End TIme</p>
       </div>
       <div className="play-control">
-        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft}/>
-        <FontAwesomeIcon className="play" size="2x" icon={faPlay}/>
-        <FontAwesomeIcon className="skip-forward" size="2x" icon={faAngleRight}/>
+        <FontAwesomeIcon className="skip-back" size="2x" icon={faAngleLeft} />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          size="2x"
+          icon={faPlay}
+        />
+        <FontAwesomeIcon
+          className="skip-forward"
+          size="2x"
+          icon={faAngleRight}
+        />
       </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
     </div>
   );
 };
